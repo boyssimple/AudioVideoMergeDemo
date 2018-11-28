107400deee2ae9db02ff894054dc7a4e3f2c8dbb
杜顺 <759266657@qq.com>
2018-11-28 14:02:44 +0800

    Initial Commit

---
+++ AudioVideoMergeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AudioVideoMergeDemo
+//
+//  Created by 杜顺 on 2018/11/28.
+//  Copyright © 2018 杜顺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AudioVideoMergeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AudioVideoMergeDemo
+//
+//  Created by 杜顺 on 2018/11/28.
+//  Copyright © 2018 杜顺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
